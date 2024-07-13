bf89fdb7be394ebae209df83951e227d674c5e52
VachhaniDeep1702 <bca2022deepkumar1702@tnraocollege.org>
2024-07-13 22:34:21 +0530

    Update as2.py

--- assignment 1/as2.py
+++ assignment 1/as2.py
@@ -1,10 +1,7 @@
-# Take input from the user
 P = float(input("Enter the principal amount: "))
 T = float(input("Enter the time in years: "))
 R = float(input("Enter the rate of interest: "))
 
-# Calculate Simple Interest
 simple_interest = (P * T * R) / 100
 
-# Print the result
 print(f"Simple Interest: {simple_interest}")
